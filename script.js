047ae5a977e0f8fe48be8986432cc6550756de31
Elijah <igundor@wgu.edu>
2020-05-12 08:11:44 -0400

    adds info & cleans up script

--- script.js
+++ script.js
@@ -5,19 +5,15 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const equalsButton = document.querySelector('[data-equals]');
 const prevNum = document.querySelector('[data-previous-operand]');
 const curNum = document.querySelector('[data-current-operand]');
+
 var operator = undefined;
-var calculation = '';
-var ready2Cal = false;
 var humanInput = false;
-var secondNumInput = false;
 var loadedOperator ='';
 
 function clear() {
   prevNum.innerText = '';
   curNum.innerText = '';
   operator = undefined;
-  ready2Cal = false;
-  secondNumInput = false;
   humanInput = false;
 }
 
@@ -42,14 +38,11 @@ function compute() {
   
   //Captures the operator
   if (prevNum.innerText == '' && curNum.innerText !== '') {
-    console.log('condition on line 44 activated');
     loadedOperator = operator;
   }
-  console.log(loadedOperator);
   
   //allows operator change when number is loaded on top w other operator
   if (prevNum.innerText != '' && curNum.innerText == '' && isOperator(loadedOperator) && operator != loadedOperator) {
-    console.log('condition on line 51 activated');
     loadedOperator = operator; 
     prevNum.innerText = prevNum.innerText.slice(0, -1) + loadedOperator;
     return
@@ -57,9 +50,8 @@ function compute() {
   
   // if statement that computes a ready state
   if (prevNum.innerText !== '' && curNum.innerText !== '') {
-    loadedOperator = prevNum.innerText.slice(-1);  //!!!!-----the loadedOperator needs to be loaded when curnum IS emmpty!
-        console.log('condition on line 58 activated');
-        
+    loadedOperator = prevNum.innerText.slice(-1); 
+
     //switch statement responsible for seeing the last operator and using it for a calculation when fields are full and new operator is pressed.
     switch (loadedOperator) {
       case '+':
@@ -85,8 +77,6 @@ function compute() {
   }
 
   if (humanInput == false) {
-    console.log('condition on line 87 acivated');
-    
     prevNum.innerText = `${curNum.innerText} ${operator}`;
     curNum.innerText = '';
     }
